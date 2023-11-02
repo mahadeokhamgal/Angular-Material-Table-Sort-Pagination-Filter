c1ffd82e22da73d6633098ddc017bb72e27213df
pratishta-shelke <pratishta.shelke>
2023-11-02 23:14:53 +0530

    mat icon displayed

--- src/app/mat4/mat4.component.ts
+++ src/app/mat4/mat4.component.ts
@@ -1,320 +1,3 @@
-
-// import { MatTableDataSource } from '@angular/material/table';
-// import { MatPaginator } from '@angular/material/paginator';
-// import { MatSort } from '@angular/material/sort';
-// import { SelectionModel } from '@angular/cdk/collections';
-// import { ResizeEvent } from 'angular-resizable-element';
-// import { FormGroup, FormControl, Validators } from '@angular/forms';
-// import { Component, Inject, ViewChild, ElementRef, Renderer2, OnInit,TemplateRef } from '@angular/core';
-// import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-
-
-
-
-// @Component({
-//   selector: 'app-mat4',
-//   templateUrl: './mat4.component.html',
-//   styleUrls: ['./mat4.component.css']
-// })
-// export class Mat4Component implements OnInit{
-//   public searchForm: FormGroup;
-//   displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol', 'action'];
-//   filterTypes: object = 
-//   {
-//     "position" : ["greater", "lesser"],
-//     "name" : ["includes", "starts", "ends", "does not contain", ""],
-//     "weight":["greater", "lesser"],
-//     "symbol":["includes", "starts", "ends", ""]
-//   };
-//   public iconVal:boolean=false;
-//   public nameFilterType = this.filterTypes["name"][0];
-//   public symbolFilterType = this.filterTypes["symbol"][0];
-//   public weightFilterType = this.filterTypes["weight"][0];
-//   public positionFilterType = this.filterTypes["position"][0];
-  
-  
-//   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-//   selection = new SelectionModel<PeriodicElement>(true, []);
-//   @ViewChild('icon') iconElement: ElementRef;
-
-//   animal: string;
-//   name: string;
- 
-//   @ViewChild('callAPIDialog1') callAPIDialog1: TemplateRef<any>;
-//   @ViewChild('callAPIDialog2') callAPIDialog2: TemplateRef<any>;
- 
-//   @ViewChild('callAPIDialog3') callAPIDialog3: TemplateRef<any>;
-//   @ViewChild('callAPIDialog4') callAPIDialog4: TemplateRef<any>;
-//   @ViewChild(MatSort, { static: true }) sort: MatSort;
-//   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-
-//   constructor(public dialog: MatDialog, private renderer: Renderer2) { }
-//   ngOnInit() {
-//     this.dataSource.sort = this.sort;
-//     this.dataSource.paginator = this.paginator;
-//     this.searchFormInit();
-//     this.dataSource.filterPredicate = this.getFilterPredicate();
-
-   
-//   }
-//   changeIconVal(){
-//     this.iconVal=true;
-//     console.log(this.iconVal);
-//   }
-//   callAPI1({ pageX, pageY, currentTarget }: MouseEvent):void {
-//     const { height, width, top, left } = (currentTarget as HTMLElement).getBoundingClientRect();
-//     console.log((currentTarget as HTMLElement).getBoundingClientRect());
-//     const dialogRef = this.dialog.open(this.callAPIDialog1,{
-      
-//         width: '350px',
-//         height:'300px',
-//         data: { name: this.name},
-//         // hasBackdrop: false,
-//         position: {
-//           // left: `${pageX}px`, top: `${pageY}px`
-//           left: `${left + width / 2}px`, top: `${top + height}px`
-//         }
-//       }
-//     );
-//     dialogRef.afterClosed().subscribe(result => {
-//       console.log('The dialog was closed');
-   
-//       this.renderer.removeClass(this.iconElement.nativeElement, 'icon-remove');
-//     });
-//   }
-//   callAPI2({ pageX, pageY, currentTarget }: MouseEvent):void {
-//     const { height, width, top, left } = (currentTarget as HTMLElement).getBoundingClientRect();
-//     console.log((currentTarget as HTMLElement).getBoundingClientRect());
-//     const dialogRef = this.dialog.open(this.callAPIDialog2,{
-      
-//         width: '350px',
-//         height:'200px',
-//         data: { name: this.name},
-//         // hasBackdrop: false,
-//         position: {
-//           // left: `${pageX}px`, top: `${pageY}px`
-//           left: `${left + width / 2}px`, top: `${top + height}px`
-//         }
-//       }
-//     );
-//     dialogRef.afterClosed().subscribe(result => {
-//       console.log('The dialog was closed');
-   
-//       this.renderer.removeClass(this.iconElement.nativeElement, 'icon-remove');
-//     });
-//   }
-//   callAPI3({ pageX, pageY, currentTarget }: MouseEvent):void {
-//     const { height, width, top, left } = (currentTarget as HTMLElement).getBoundingClientRect();
-//     console.log((currentTarget as HTMLElement).getBoundingClientRect());
-//     const dialogRef = this.dialog.open(this.callAPIDialog3,{
-      
-//         width: '350px',
-//         height:'200px',
-//         data: { name: this.name},
-//         // hasBackdrop: false,
-//         position: {
-//           // left: `${pageX}px`, top: `${pageY}px`
-//           left: `${left + width / 2}px`, top: `${top + height}px`
-//         }
-//       }
-//     );
-//     dialogRef.afterClosed().subscribe(result => {
-//       console.log('The dialog was closed');
-   
-//       this.renderer.removeClass(this.iconElement.nativeElement, 'icon-remove');
-//     });
-//   }
-//   callAPI4({ pageX, pageY, currentTarget }: MouseEvent):void {
-//     const { height, width, top, left } = (currentTarget as HTMLElement).getBoundingClientRect();
-//     console.log((currentTarget as HTMLElement).getBoundingClientRect());
-//     const dialogRef = this.dialog.open(this.callAPIDialog4,{
-      
-//         width: '350px',
-//         height:'200px',
-//         data: { name: this.name},
-//         // hasBackdrop: false,
-//         position: {
-//           // left: `${pageX}px`, top: `${pageY}px`
-//           left: `${left + width / 2}px`, top: `${top + height}px`
-//         }
-//       }
-//     );
-//     dialogRef.afterClosed().subscribe(result => {
-//       console.log('The dialog was closed');
-   
-//       this.renderer.removeClass(this.iconElement.nativeElement, 'icon-remove');
-//     });
-//   }
-//   onFilter():void{
-
-//   }
-//   onCancel(): void { 
-//     this.dialog.closeAll(); 
-//   } 
-//   isAllSelected() {
-//     return this.selection.selected.length === this.dataSource.data.length;
-//   }
-
-//   masterToggle() {
-//     this.isAllSelected()
-//       ? this.selection.clear()
-//       : this.dataSource.data.forEach((row) => this.selection.select(row));
-//   }
-  
-//   // openDialog({ pageX, pageY, currentTarget }: MouseEvent): void {
-  
-//   //   console.log((currentTarget as HTMLElement).getBoundingClientRect());
-//   //   const dialogRef = this.dialog.open(ExampleDialogComponentComponent, {
-//   //     width: '350px',
-//   //     data: { name: this.name, positionFilterType: this.positionFilterType },
-//   //     // hasBackdrop: false,
-//   //     position: {
-//   //       // left: `${pageX}px`, top: `${pageY}px`
-//   //       left: `${left + width / 2}px`, top: `${top + height}px`
-//   //     }
-//   //   }
-//   //   );
-
-//   //   dialogRef.afterClosed().subscribe(result => {
-//   //     console.log('The dialog was closed');
-   
-//   //     this.renderer.removeClass(this.iconElement.nativeElement, 'icon-remove');
-//   //   });
-
-//   //   console.log(this.iconElement);
-//   //   // console.log(this.matIcon);
-//   //   // this.matIcon.color = 'accent';
-//   //   this.renderer.addClass(this.iconElement.nativeElement, 'icon-remove');
-//   // }
-
-
-
-//   onResizeEnd(event: ResizeEvent, columnName): void {
-//     if (event.edges.right) {
-//       const cssValue = event.rectangle.width + 'px';
-//       const columnElts = document.getElementsByClassName(
-//         'mat-column-' + columnName
-//       );
-//       for (let i = 0; i < columnElts.length; i++) {
-//         const currentEl = columnElts[i] as HTMLDivElement;
-//         currentEl.style.width = cssValue;
-//       }
-//     }
-//   }
-//   searchFormInit() {
-//     this.searchForm = new FormGroup({
-//       position: new FormControl(0),
-//       name: new FormControl('', Validators.pattern('^[a-zA-Z ]+$')),
-//       weight: new FormControl(0),
-//       symbol: new FormControl('', Validators.pattern('^[a-zA-Z ]+$')),
-//     });
-//   }
-//   getTypeVal(colVal, inVal, filterType): boolean {
-//     console.log("gettype val", colVal, inVal, filterType);
-//     if(inVal===""|| inVal===0 || inVal===null)
-//       return true;
-    
-//     switch (filterType) {
-//       case "": return true;
-//       case "includes":  return colVal.toLowerCase().includes(inVal);
-//       case "starts": return colVal.toLowerCase().startsWith(inVal);
-//       case "ends": return colVal.toLowerCase().endsWith(inVal);
-//       case "does not contain": return !colVal.toLowerCase().includes(inVal);
-//       case "greater": return parseFloat(colVal) > parseFloat(inVal);
-//       case "lesser": return parseFloat(colVal) < parseFloat(inVal);
-//     }
-//     return true;
-//   }
-//   getFilterPredicate() {
-//     return (row: PeriodicElement, filters: string) => {
-//       const filterArray = filters.split('$');
-      
-//       return this.getTypeVal(row.position, filterArray[0], this.positionFilterType)
-//         && this.getTypeVal(row.name, filterArray[1], this.nameFilterType)
-//         && this.getTypeVal(row.weight, filterArray[2], this.weightFilterType)
-//         && this.getTypeVal(row.symbol, filterArray[3], this.symbolFilterType);
-//     };
-//   }
-//   applyFilter() {
-//     var position = this.searchForm.get('position').value;
-//     var name = this.searchForm.get('name').value;
-//     var weight = this.searchForm.get('weight').value;
-//     var symbol = this.searchForm.get('symbol').value;
-    
-//     position = position === null ? '' : position;
-//     name = name === null ? '' : name;
-//     weight = weight === null ? '' : weight;
-//     symbol = symbol === null ? '' : symbol;
-
-//     this.dataSource.filter = (position + "$" + name + '$' + weight + "$" + symbol).trim().toLowerCase();
-//   }
-
-//   selectedRow;
-// }
-
-// export interface PeriodicElement {
-//   name: string;
-//   position: number;
-//   weight: number;
-//   symbol: string;
-// }
-// const Quote_Data:any=
-// {
-//   "quoteId": 64837,
-//   "name": "mkhamgal - Untitled",
-//   "status": null,
-//   "cfu": "OB",
-//   "totalUpfront": "0",
-//   "totalMonthly": "0",
-//   "createdBy": "P4002578",
-//   "createdDate": "2023-10-20T22:31:39",
-//   "updatedBy": "P4002578",
-//   "updatedDate": "2023-10-20T22:31:58",
-//   "createdByUser": "Mahadeo Khamgal",
-//   "updatedByUser": "Mahadeo Khamgal",
-//   "customer": {
-//       "uid": "43884",
-//       "name": "BRT Customer - Test"
-//   },
-//   "opportunity": {
-//       "opportunityId": "1-TEST12"
-//   },
-//   "contract": {
-//       "agreementType": "NEW",
-//       "agreementId": null,
-//       "agreementStatus": "Quote Created"
-//   },
-//   "avgArpu": null,
-//   "avgEarpu": null,
-//   "productId": null,
-//   "version": "1.5000000000000004",
-//   "approvalSummary": null
-// }
-
-// const ELEMENT_DATA: PeriodicElement[] = [
-//   { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-//   { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-//   { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-//   { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-//   { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-//   { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-//   { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-//   { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-//   { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-//   { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-//   { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-//   { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-//   { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-//   { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-//   { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-//   { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-//   { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-//   { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-//   { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-//   { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
-// ];
-
-
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
@@ -555,7 +238,131 @@ const QUOTE_DATA:any[] = [
       "productId": null,
       "version": "1",
       "approvalSummary": null
-  }
+  },
+  {
+    "quoteId": 64051,
+    "name": "dsfjf - Untitled",
+    "status": "null",
+    "cfu": "OB",
+    "totalUpfront": "0",
+    "totalMonthly": "0",
+    "createdBy": "p4003712",
+    "createdDate": "2023-10-16T15:53:25",
+    "updatedBy": "p4003712",
+    "updatedDate": "2023-10-16T15:53:25",
+    "createdByUser": "Mohammed Hundekar",
+    "updatedByUser": "Mohammed Hundekar",
+    "customer": {
+        "uid": "43884",
+        "name": "fdhd Customer - Victoria"
+    },
+    "opportunity": {
+        "opportunityId": "1-7N0R11"
+    },
+    "contract": {
+        "agreementType": "NEW",
+        "agreementId": null,
+        "agreementStatus": "Quote Created"
+    },
+    "avgArpu": null,
+    "avgEarpu": null,
+    "productId": null,
+    "version": "1",
+    "approvalSummary": null
+},
+{
+  "quoteId": 64052,
+  "name": "fnn - Untitled",
+  "status": "null",
+  "cfu": "OB",
+  "totalUpfront": "0",
+  "totalMonthly": "0",
+  "createdBy": "p4003712",
+  "createdDate": "2023-10-16T15:53:25",
+  "updatedBy": "p4003712",
+  "updatedDate": "2023-10-16T15:53:25",
+  "createdByUser": "Mohammed Hundekar",
+  "updatedByUser": "Mohammed Hundekar",
+  "customer": {
+      "uid": "43884",
+      "name": "kjjgk Customer - Victoria"
+  },
+  "opportunity": {
+      "opportunityId": "1-7N0R12"
+  },
+  "contract": {
+      "agreementType": "resign",
+      "agreementId": null,
+      "agreementStatus": "Quote Created"
+  },
+  "avgArpu": null,
+  "avgEarpu": null,
+  "productId": null,
+  "version": "1",
+  "approvalSummary": null
+},
+{
+  "quoteId": 64053,
+  "name": "dfkkr - Untitled",
+  "status": "null",
+  "cfu": "OB",
+  "totalUpfront": "0",
+  "totalMonthly": "0",
+  "createdBy": "p4003712",
+  "createdDate": "2023-10-16T15:53:25",
+  "updatedBy": "p4003712",
+  "updatedDate": "2023-10-16T15:53:25",
+  "createdByUser": "Mohammed Hundekar",
+  "updatedByUser": "Mohammed Hundekar",
+  "customer": {
+      "uid": "43884",
+      "name": "axsda Customer - Victoria"
+  },
+  "opportunity": {
+      "opportunityId": "1-7N0R13"
+  },
+  "contract": {
+      "agreementType": "MACT",
+      "agreementId": null,
+      "agreementStatus": "Quote Created"
+  },
+  "avgArpu": null,
+  "avgEarpu": null,
+  "productId": null,
+  "version": "1",
+  "approvalSummary": null
+},
+{
+  "quoteId": 64054,
+  "name": "pdfdf - Untitled",
+  "status": "null",
+  "cfu": "OB",
+  "totalUpfront": "0",
+  "totalMonthly": "0",
+  "createdBy": "p4003712",
+  "createdDate": "2023-10-16T15:53:25",
+  "updatedBy": "p4003712",
+  "updatedDate": "2023-10-16T15:53:25",
+  "createdByUser": "Mohammed Hundekar",
+  "updatedByUser": "Mohammed Hundekar",
+  "customer": {
+      "uid": "43884",
+      "name": "pdsp Customer - Victoria"
+  },
+  "opportunity": {
+      "opportunityId": "1-7N0R14"
+  },
+  "contract": {
+      "agreementType": "Resign",
+      "agreementId": null,
+      "agreementStatus": "Quote Created"
+  },
+  "avgArpu": null,
+  "avgEarpu": null,
+  "productId": null,
+  "version": "1",
+  "approvalSummary": null
+}
 ];
 
 
